4dc766fa79ff8d4664cc68505e0e127153197382
notenote087 <nonthawat.ro.57@ubu.ac.th>
2017-07-12 00:00:55 +0700

    OK Speak And Translate Update Ui and Speak Sound Thai

--- app/src/main/java/com/example/admin/stickspeak/Main2Activity.java
+++ app/src/main/java/com/example/admin/stickspeak/Main2Activity.java
@@ -37,7 +37,7 @@ public class Main2Activity extends AppCompatActivity {
     TextView textView;
     ProgressDialog progressDialog;
 
-    TextToSpeech sp ;
+    TextToSpeech sp ; //""
     int result ;
     String speak = "";
 
